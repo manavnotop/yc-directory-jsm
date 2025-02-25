c2a24820a465af68946e3286d8266a1e3620bd20
manavnotop <manuagarwal1443@gmail.com>
2025-02-25 18:01:52 +0530

    Add: Search form to landing page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,11 @@
+import SearchForm from "@/components/SearchForm";
 
-export default function Home() {
+export default async function Home({searchParams}: {
+  searchParams: Promise<{ query?: string }>
+}) {
+
+  const query = (await searchParams).query
+
   return (
     <section className="pink_container">
       <h1 className="heading">
@@ -8,6 +14,7 @@ export default function Home() {
       <p className="sub-heading !max-w-3xl">
         Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions
       </p>
+      <SearchForm query={query} />
     </section>
   )
 }
