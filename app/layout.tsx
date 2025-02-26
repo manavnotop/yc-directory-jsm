c4004866ec112371cb5947971c32b0b6de1c05d0
manavnotop <manuagarwal1443@gmail.com>
2025-02-26 20:25:29 +0530

    Add: Sanity setup complete

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Work_Sans } from "next/font/google";
 import localFont from "next/font/local";
 import "./globals.css";
+// import 'easymde/dist/easymde.min.css'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
